534eedd83f473be8b23a537617bf4ed8f0493903
Ivan Mendez <ivan.mendez.tlg@gmail.com>
2017-10-20 10:13:47 +0200

    commit manual desde cmd

--- Test Suites/TestSuiteGit.ts
+++ Test Suites/TestSuiteGit.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>eeb8a7fb-56c1-4857-a650-0dbbc4d5c37c</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ce184d7-e354-418d-8792-26f611508aeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestInicial</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>763a35f9-c338-4f28-9e07-a67dfbf8d357</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestInicialRecord</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
