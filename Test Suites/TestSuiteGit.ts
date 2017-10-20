e6002b559571762aac630323b7a45b8304c168f9
angel <angel@W10-PRJ>
2017-10-20 09:47:01 +0200

    Commit cuando he añadido ya un testconRecord

---
+++ Test Suites/TestSuiteGit.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteGit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>eeb8a7fb-56c1-4857-a650-0dbbc4d5c37c</testSuiteGuid>
+</TestSuiteEntity>
