d51470c3d6635a8267cc5bbe4a72107968391182
Ivan Mendez <ivan.mendez.tlg@gmail.com>
2017-10-24 10:11:04 +0200

    Commit desde Jenkins

--- Test Suites/TestSuiteGit.ts
+++ Test Suites/TestSuiteGit.ts
@@ -4,7 +4,7 @@
    <name>TestSuiteGit</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-10-24T10:05:50</lastRun>
+   <lastRun>2017-10-24T10:09:17</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
